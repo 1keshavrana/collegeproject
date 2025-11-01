1bd0e6674ef2a5846de7189010473c3a69ce1d9b
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-11-01 10:35:31 +0530

    backend-i

--- app.js
+++ app.js
@@ -1,40 +1,167 @@
 const express = require("express");
 const app = express();
-const mongooose = require("mongoose");
-const Listing = require("./models/listing")
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(
-    ()=>{
-        console.log("connected to db");
-    }
-).catch(err => {
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongooose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
-app.get("/",(req,res)=>{
-    res.send("hi, im root");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
-app.get("/testlisting",async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description:"By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
-app.listen(8080,() =>{
-    console.log("server is listing");
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
+
+
+
+
+
+
+// // const express = require("express");
+// // const app = express();
+// // const mongooose = require("mongoose");
+// // const Listing = require("./models/listing")
+
+// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+// // main().then(
+// //     ()=>{
+// //         console.log("connected to db");
+// //     }
+// // ).catch(err => {
+// //     console.log(err);
+// // });
+
+// // async function main() {
+// //     await mongooose.connect(MONGO_URL);
+// // }
+
+// // app.get("/",(req,res)=>{
+// //     res.send("hi, im root");
+// // });
+
+// // app.get("/testlisting",async (req,res)=>{
+// //     let sampleListing = new Listing({
+// //         title: "My New Villa",
+// //         description:"By the beach",
+// //         price: 1200,
+// //         location: "Calangute, Goa",
+// //         country: "India"
+// //     });
+
+// //     await sampleListing.save();
+// //     console.log("sample was saved");
+// //     res.send("successful testing");
+// // });
+
+// // app.listen(8080,() =>{
+// //     console.log("server is listing");
+// // });
+
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   description: String,
+//   image: {
+//     type: String,
+//     default:
+//       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     set: (v) =>
+//       v === ""
+//         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//         : v,
+//   },
+//   price: Number,
+//   location: String,
+//   country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,28 @@
-const mongooose = require("mongoose");
-const Schema = mongooose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title :{
-        type: String,
-        required : true,
-    },
-    description: String,
-    image: {
-        type: String,
-        set : (v) => v === ""?"https://media.istockphoto.com/id/930082108/photo/solitary-lime-tree-in-fields-of-rapeseed-and-wheat-under-blue-sky.jpg?s=612x612&w=0&k=20&c=ALuNL8rkWjxYu8aCytJAoW13AdYC2Hc2DIgFbNQ6KcU=" : v,
-    },
-    price : Number,
-    location : String,
-    country : String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/930082108/photo/solitary-lime-tree-in-fields-of-rapeseed-and-wheat-under-blue-sky.jpg?s=612x612&w=0&k=20&c=ALuNL8rkWjxYu8aCytJAoW13AdYC2Hc2DIgFbNQ6KcU=",
+      set: (v) =>
+        v === ""
+          ? "https://media.istockphoto.com/id/930082108/photo/solitary-lime-tree-in-fields-of-rapeseed-and-wheat-under-blue-sky.jpg?s=612x612&w=0&k=20&c=ALuNL8rkWjxYu8aCytJAoW13AdYC2Hc2DIgFbNQ6KcU="
+          : v,
+    }
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = mongooose.model("Listing",listingSchema);
-module.export = Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
